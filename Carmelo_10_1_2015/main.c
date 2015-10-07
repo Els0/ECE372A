de55900832f17eaa4087b2dec580f6150cc324af
Carmelo Moraila <cmoraila5@dhcp-10-142-249-165.uawifi.arizona.edu>
2015-10-06 17:56:47 -0700

    PART_DONE

--- Carmelo_10_1_2015/main.c
+++ Carmelo_10_1_2015/main.c
@@ -19,11 +19,17 @@
 
 int main(void)
 {
+    SYSTEMConfigPerformance(40000000);
+    
+    initTimer2();
     initLCD();
 
     while(1)
     {
         testLCD();
+        //        writeFourBits(0b10100000, 0, 4100, 0);
+//      for(i = 0; i < 1000; i++) delayUs(1000);
+//      LATDbits.LATD0 ^= 1;
     }
     
     return 0;
